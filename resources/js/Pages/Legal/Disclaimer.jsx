0914026aebf53f04b35199dcdfc6ca6da465784b
Mélanie Roussy <malachipito@protonmail.com>
2024-01-17 21:23:22 +0100

    Improving disclaimer content

--- resources/js/Pages/Legal/Disclaimer.jsx
+++ resources/js/Pages/Legal/Disclaimer.jsx
@@ -8,15 +8,16 @@ const Disclaimer = () => {
 
             <section className="mb-8">
                 <h2 className="text-2xl font-bold mb-2">Information éditeur</h2>
-                <p>Les principaux services du ministère participent à la gestion et à l’amélioration du site ressources-relationnelles.fr.<br/>
-                    Le suivi éditorial, graphique et technique est pris en charge par le cabinet du ministre et par la délégation à l’information et à la communication (DICOM).<br/>
+                <p>Les directions et services du ministère participent à la gestion et à l’amélioration du site ressources-relationnelles.fr.<br/>
+                    Le suivi éditorial, graphique et technique est pris en charge par la délégation à l’information et à la communication (DICOM).<br/>
                     14 avenue Duquesne - 75350 Paris 07 SP.</p>
             </section>
 
             <section className="mb-8">
                 <h2 className="text-2xl font-bold mb-2">Informations prestataires</h2>
                 <p>L'hébergement est assuré par la société <a href="https://fr.wikipedia.org/" className="font-bold">OVHCLoud</a>.<br/>
-                    Le développement est assuré par la société <a href="https://fr.wikipedia.org/" className="font-bold">MNA Coding</a>.</p>
+                    Le développement est assuré par la société <a href="https://fr.wikipedia.org/" className="font-bold">MNA Coding</a>.<br/>
+                    Le conseil en accessibilité est assuré par la société <a href="https://fr.wikipedia.org/" className="font-bold">Koena</a>.</p>
             </section>
 
             <section className="mb-8">
@@ -39,24 +40,48 @@ const Disclaimer = () => {
             </section>
 
             <section className="mb-8">
+                <h2 className="text-2xl font-bold mb-2">Données personelles</h2>
+                <p>Lorsque des données présentes sur ce site ont un caractère nominatif, les utilisateurs doivent en faire un usage conforme
+                    aux réglementations en vigueur et aux recommandations de la Commission nationale de l'informatique et des libertés (CNIL).< br/>
+                    Conformément au règlement général sur la protection des données (RGPD) et à la loi Informatique et Liberté 78-17 du 6 janvier 1978 modifiée,
+                    vous disposez d'un droit d'opposition (art. 38), d'accès (art. 39), de rectification ou de suppression (art. 40) des données vous concernant.
+                    Vous pouvez exercer ce droit en vous adressant au ministère des Affaires Sociales et de la Santé.< br/>
+                    - Consulter la page <a href="https://fr.wikipedia.org/" className="font-bold">Politique de confidentialité</a>.<br/>
+                    - Consulter la page <a href="https://fr.wikipedia.org/" className="font-bold">Politique de cookies</a>.
+                </p>
+            </section>
+
+            <section className="mb-8">
                 <h2 className="text-2xl font-bold mb-2">Propriété intellectuelle</h2>
-                <p>L’intégralité des pages, textes, images et graphiques présentent sur le site sont protégés par droit d’auteur ou d’autres droits de protection prévus par la loi.< br/>
-                    Sauf accord explicite, aucune licence n’est accordée au titre du présent site.< br/>
-                    Il est interdit de copier, diffuser ou modifier tout ou partie du contenu du présent site à des fins commerciales ou autre et d’en permettre l’accès à des tiers sans autorisation préalable écrite.< br/>
-                    Nous attirons l’attention sur le fait que le site contient des images pouvant être soumises au droit d’auteur de tiers.</p>
+                <h3 className="text-2xl font-bold mb-2">Contenus publiés par le ministère</h3>
+                <p>Les ressources relationnelles mises en ligne par le ministère sur le site ressources-relationnelles, à l'exception des marques, logos, contenus grevés de droits de propriété intellectuelle de tiers
+                    ou contenant des données à caractère personnel, sont des informations publiques qui peuvent être réutilisées librement et gratuitement.< br/>
+                    Les modalités de réutilisation sont définies par la loi n°78-753 du 17 juillet 1978, ainsi que par les conditions générales de réutilisation des informations publiques.< br/>
+                    Si vous envisagez de réutiliser des contenus disponibles sur ce site, veuillez nous <a href="https://fr.wikipedia.org/" className="font-bold">nous contacter</a> pour obtenir les autorisations nécessaires.< br/></p>
+                <h3 className="text-2xl font-bold mb-2">Contenus publiés par les utilisateurs</h3>
+                <p>Les utilisateurs garantissent qu'ils détiennent les droits nécessaires sur les textes et images qu'ils publient et que leur utilisation ne viole pas les droits de tiers.< br/>
+                    Ils sont entièrement responsables de tout litige résultant de l'utilisation de contenus soumis.< br/>
+                    Le ministère des Solidarités et des Familles  se réserve le droit de retirer, sans préavis, tout contenu qui enfreint les droits de propriété intellectuelle d'autrui.</p>
             </section>
 
             <section className="mb-8">
                 <h2 className="text-2xl font-bold mb-2">Responsabilités relatives aux contenus</h2>
-                <h3 className="text-2xl font-bold mb-2">Contenus du site</h3>
-                <p>Malgré tout le soin apporté par nos équipes éditoriales et techniques à la rédaction,
+                <h3 className="text-2xl font-bold mb-2">Contenus publiés par le ministère</h3>
+                <p>Malgré toute l'attention apportée par nos équipes éditoriales et techniques à la rédaction,
                     des erreurs typographiques ou des inexactitudes techniques ne peuvent être exclues.<br/>
-                    Le ministère de la Santé et de la Prévention se réserve le droit de les corriger à tout moment dès qu’elles sont portées à sa connaissance.<br/>
-                    Les informations présentent sur le site sont susceptibles de faire l’objet de mise à jour à tout moment.</p>
-                <h3 className="text-2xl font-bold mb-2">Clause de responsabilité</h3>
-                <p>Bien que nous apportions une attention particulière à la transcription des textes officiels
-                    et à la vérification des contenus et des informations, les éléments publiés ne peuvent en aucun cas
+                    Le ministère des Solidarités et des Familles se réserve le droit de les corriger à tout moment dès qu’elles sont portées à sa connaissance.<br/>
+                    Les informations présentent sur le site sont susceptibles de faire l’objet de mise à jour à tout moment.<br/>
+
+                    Bien que nous apportions une attention particulière à la vérification des contenus et des informations, les éléments publiés ne peuvent en aucun cas
                     prétendre à une exactitude totale et n'engagent pas la responsabilité du ministère.</p>
+
+                <h3 className="text-2xl font-bold mb-2">Contenus publiés par les utilisateurs</h3>
+                    <p>En ce qui concerne les contenus publiés par les utilisateurs, le ministère des Solidarités et des Familles décline toute responsabilité.<br/>
+                        Le ministère ne garantit pas l'exactitude, la légalité ou la conformité de ces contenus, et il ne peut être tenu responsable des éventuels litiges découlant de leur utilisation.<br/>
+                        Le ministère se réserve le droit de retirer, sans préavis, tout contenu publié par les utilisateurs s'il les lois applicables ou s'il est jugé inapproprié.<br/>
+                        En utilisant ce site, les utilisateurs reconnaissent et acceptent que le ministère des Solidarités et des Familles ne soit pas responsable des contenus générés par les utilisateurs, et ils libèrent le ministère
+                        de toute responsabilité liée à ces contenus.
+                    </p>
             </section>
         </div>
     );
