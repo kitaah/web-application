46c7d92f8faae97fd63c51b0fab706c01aea966b
alexandre pinhal <alexandre.pinhal@outlook.com>
2024-05-10 18:09:46 +0200

    Fixed css logo

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -28,82 +28,116 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <Layout header={<h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">Se connecter</h2>}>
+        <Layout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">
+                    Se connecter
+                </h2>
+            }
+        >
             <div className="container mx-auto my-8">
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">Connexion</h2>
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">
+                    Connexion
+                </h2>
 
                 <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-                    <div>
+                    <div className="h-10">
                         <Link href="/">
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                            <ApplicationLogo className="w-50 fill-current text-gray-500" />
                         </Link>
                     </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                        {status && (
+                            <div className="mb-4 font-medium text-sm text-green-600">
+                                {status}
+                            </div>
+                        )}
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+                        <form onSubmit={submit}>
+                            <div>
+                                <InputLabel htmlFor="email" value="Email" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 block w-full"
+                                    autoComplete="username"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("email", e.target.value)
+                                    }
+                                />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+                                <InputError
+                                    message={errors.email}
+                                    className="mt-2"
+                                />
+                            </div>
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+                            <div className="mt-4">
+                                <InputLabel
+                                    htmlFor="password"
+                                    value="Password"
+                                />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-1 block w-full"
+                                    autoComplete="current-password"
+                                    onChange={(e) =>
+                                        setData("password", e.target.value)
+                                    }
+                                />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
+                                <InputError
+                                    message={errors.password}
+                                    className="mt-2"
+                                />
+                            </div>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+                            <div className="block mt-4">
+                                <label className="flex items-center">
+                                    <Checkbox
+                                        name="remember"
+                                        checked={data.remember}
+                                        onChange={(e) =>
+                                            setData(
+                                                "remember",
+                                                e.target.checked
+                                            )
+                                        }
+                                    />
+                                    <span className="ms-2 text-sm text-gray-600">
+                                        Remember me
+                                    </span>
+                                </label>
+                            </div>
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
+                            <div className="flex items-center justify-end mt-4">
+                                {canResetPassword && (
+                                    <Link
+                                        href={route("register")}
+                                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    >
+                                        Pas inscrit ? Créer un compte
+                                    </Link>
+                                )}
 
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('register')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Pas inscrit ? Créer un compte
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-            </div>
+                                <PrimaryButton
+                                    className="ms-4"
+                                    disabled={processing}
+                                >
+                                    Log in
+                                </PrimaryButton>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </Layout>
--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,8 +1,8 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import PrimaryButton from '@/Components/PrimaryButton';
-import { Head, Link, useForm } from '@inertiajs/react';
+import GuestLayout from "@/Layouts/GuestLayout";
+import PrimaryButton from "@/Components/PrimaryButton";
+import { Head, Link, useForm } from "@inertiajs/react";
 import ApplicationLogo from "@/Components/ApplicationLogo.jsx";
-import Layout from '@/Layouts/Layout';
+import Layout from "@/Layouts/Layout";
 
 export default function VerifyEmail({ status }) {
     const { post, processing } = useForm({});
@@ -10,49 +10,62 @@ export default function VerifyEmail({ status }) {
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('verification.send'));
+        post(route("verification.send"));
     };
 
     return (
-        <Layout header={<h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">S'inscrire</h2>}>
+        <Layout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">
+                    S'inscrire
+                </h2>
+            }
+        >
             <div className="container mx-auto my-8">
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">Inscription</h2>
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center">
+                    Inscription
+                </h2>
 
                 <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-                    <div>
+                    <div className="h-10">
                         <Link href="/">
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                            <ApplicationLogo className="w-50 fill-current text-gray-500" />
                         </Link>
                     </div>
                     <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <Head title="Email Verification" />
+                        <Head title="Email Verification" />
 
-            <div className="mb-4 text-sm text-gray-600">
-                Un email de validation de votre compte vient de vous être envoyé,
-                merci de le consulter pour valider votre inscription. Si vous n'avez pas reçu de lien, veuillez cliquer
-                sur le bouton ci-dessous pour en recevoir un autre.
-            </div>
+                        <div className="mb-4 text-sm text-gray-600">
+                            Un email de validation de votre compte vient de vous
+                            être envoyé, merci de le consulter pour valider
+                            votre inscription. Si vous n'avez pas reçu de lien,
+                            veuillez cliquer sur le bouton ci-dessous pour en
+                            recevoir un autre.
+                        </div>
 
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    Un nouvel email de vérification vient de vous être envoyé.
-                </div>
-            )}
+                        {status === "verification-link-sent" && (
+                            <div className="mb-4 font-medium text-sm text-green-600">
+                                Un nouvel email de vérification vient de vous
+                                être envoyé.
+                            </div>
+                        )}
 
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>Renvoyer le lien de vérification</PrimaryButton>
+                        <form onSubmit={submit}>
+                            <div className="mt-4 flex items-center justify-between">
+                                <PrimaryButton disabled={processing}>
+                                    Renvoyer le lien de vérification
+                                </PrimaryButton>
 
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Accueil
-                    </Link>
-                </div>
-            </form>
+                                <Link
+                                    href={route("logout")}
+                                    method="post"
+                                    as="button"
+                                    className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    Accueil
+                                </Link>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
