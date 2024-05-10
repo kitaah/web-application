86b5bba69317e47dd55e48bb884f54a38ab9ec0e
alexandre pinhal <alexandre.pinhal@outlook.com>
2024-05-10 14:43:00 +0200

    changed logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,9 +1,14 @@
 import logoMarianne from "../../../public/assets/Logo_Marianne.svg";
+import logo from "../../../public/assets/RessourcesRelationnelles.jpg";
 
 export default function ApplicationLogo(props) {
     return (
-        <div>
-            <img src={logoMarianne} alt="Logo Marianne" />
+        <div className={props.className}>
+            <img
+                style={{ maxHeight: "100%" }}
+                src={logo}
+                alt="Logo Ressources Relationnelles"
+            />
         </div>
     );
 }
